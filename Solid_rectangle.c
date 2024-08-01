6d5006c077cb7bc7fbb3c76cbb9c1ff26e0c2f78
Hritik Shingare <hruthikshingare1@gmail.com>
2024-08-01 16:08:36 +0530

    Add files via upload

---
+++ Solid_rectangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{   
+    for (int i = 0; i < 10; i++)
+   {
+    
+    for (int i = 0; i < 10; i++)
+    { 
+        printf("*");
+
+    }printf("\n");
+   }
+    return 0;
+}
